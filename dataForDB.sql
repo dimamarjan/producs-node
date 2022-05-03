7b6cd55311c1e01b7eaae004401e249566a5440f
Dmitriy Marianchenko <dima.marjan@gmail.com>
2022-05-03 15:25:12 +0300

    fix

--- dataForDB.sql
+++ dataForDB.sql
@@ -24,17 +24,23 @@ CREATE TABLE "Products"
     "CategoryID" INTEGER REFERENCES "Categories" ("CategoryID") ON DELETE NO ACTION ON UPDATE CASCADE
 );
 
-INSERT INTO "Suppliers" ("SupplierName", "City", "Country") VALUES ('Exotic Liquid', 'London', 'UK') RETURNING *;
-INSERT INTO "Suppliers" ("SupplierName", "City", "Country") VALUES ('New Orleans Cajun Delights', 'New Orleans', 'USA') RETURNING *;
-INSERT INTO "Suppliers" ("SupplierName", "City", "Country") VALUES ('Grandma Kelly’s Homestead', 'Ann Arbor', 'USA') RETURNING *;
-INSERT INTO "Suppliers" ("SupplierName", "City", "Country") VALUES ('Tokyo Traders', 'Tokyo', 'Japan') RETURNING *;
-INSERT INTO "Suppliers" ("SupplierName", "City", "Country") VALUES ('Cooperativa de Quesos ‘Las Cabras’', 'Oviedo', 'Spain') RETURNING *;
-INSERT INTO "Suppliers" ("SupplierName", "City", "Country") VALUES ('Norske Meierier’', 'Lviv', 'Ukraine') RETURNING *;
+INSERT INTO "Suppliers" ("SupplierName", "City", "Country")
+VALUES 
+('Exotic Liquid', 'London', 'UK'),
+('New Orleans Cajun Delights', 'New Orleans', 'USA'),
+('Grandma Kelly’s Homestead', 'Ann Arbor', 'USA'),
+('Tokyo Traders', 'Tokyo', 'Japan'),
+('Cooperativa de Quesos ‘Las Cabras’', 'Oviedo', 'Spain'),
+('Norske Meierier’', 'Lviv', 'Ukraine');
 
-INSERT INTO "Categories" ("CategoryName", "Description") VALUES ('Beverages', 'Soft drinks, coffees, teas, beers, and ales') RETURNING *;
-INSERT INTO "Categories" ("CategoryName", "Description") VALUES ('Condiments', 'Sweet and savory sauces, relishes, spreads, and seasonings') RETURNING *;
-INSERT INTO "Categories" ("CategoryName", "Description") VALUES ('Confections', 'Desserts, candies, and sweet breads') RETURNING *;
-INSERT INTO "Categories" ("CategoryName", "Description") VALUES ('Dairy Products', 'Cheeses') RETURNING *;
-INSERT INTO "Categories" ("CategoryName", "Description") VALUES ('Grains/Cereals', 'Breads, crackers, pasta, and cereal') RETURNING *;
+INSERT INTO "Categories" ("CategoryName", "Description") 
+VALUES 
+('Beverages', 'Soft drinks, coffees, teas, beers, and ales'),
+('Condiments', 'Sweet and savory sauces, relishes, spreads, and seasonings'),
+('Confections', 'Desserts, candies, and sweet breads'),
+('Dairy Products', 'Cheeses'),
+('Grains/Cereals', 'Breads, crackers, pasta, and cereal');
 
-INSERT INTO "Products" ("ProductName","Price","SupplierID","CategoryID") VALUES ('Green tea', '10', '6','1') RETURNING *;
+INSERT INTO "Products" ("ProductName","Price","SupplierID","CategoryID") 
+VALUES 
+('Green tea', 10, '6','1');
